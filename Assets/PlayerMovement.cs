4ae4e514a2485babbd72d1aceca74a8c00df4218
Shaf Liaquat <shaf712@outlook.com>
2018-10-14 19:46:26 +0000

    Work on player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
