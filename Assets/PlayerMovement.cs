48a51dbec62730d47f086f81a53ea7525dd43347
Shaf Liaquat <shaf712@outlook.com>
2018-10-14 20:33:24 +0000

    Working on player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	
+	public Rigidbody rb;
+
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		rb.AddForce(0, 0, 2000 * Time.deltaTime);
 	}
 }
